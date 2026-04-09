37f9aefd7162e89dd54349d742d9eaff3d5de69a
sha0001 <sha0001@gmail.com>
2026-04-09 22:21:59 +1000

    Initial upload before I break it

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import argparse
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key == None: 
+    raise RuntimeError ('Environment variable api_key not found')
+
+client = genai.Client(api_key=api_key)
+
+
+
+
+
+prompt ='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+response = client.models.generate_content(
+        model='gemini-2.5-flash', contents=prompt
+        )
+
+if response.usage_metadata == None: 
+    raise RuntimeError('missing usage_metadata - failed API request?')
+else: 
+    #print the outputs 
+    print(f'User prompt: {prompt}')
+
+    print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}') 
+    print(f'Response tokens: {response.usage_metadata.candidates_token_count}')
+
+    print(f'Response: \n {response.text}')
